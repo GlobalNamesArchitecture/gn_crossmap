2cfe2813d8d3a9cd6cd67bad211de8a51c0d8eba
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-05-10 21:52:08 -0400

    add features specs

---
+++ spec/features/checklist_resolver_spec.rb
@@ -0,0 +1,13 @@
+describe "resolving variety of csv files" do
+  %i(all_fields sciname sciname_auth sciname_rank).each do |input|
+    context input do
+      it "resolves #{input}" do
+        output = "/tmp/#{input}-processed.csv"
+        input = FILES[input]
+        FileUtils.rm(output) if File.exist?(output)
+        GnCrossmap.run(input, output, 1)
+        expect(File.exist?(output)).to be true
+      end
+    end
+  end
+end
--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -2,5 +2,8 @@ files_path = File.expand_path("../files", __dir__)
 
 FILES = {
   all_fields: "#{files_path}/all-fields-semicolon.csv",
+  sciname: "#{files_path}/simple-comma.csv",
+  sciname_auth: "#{files_path}/authorship-tab.csv",
+  sciname_rank: "#{files_path}/taxon-rank-tab.csv",
   output: "/tmp/output.csv"
 }
