6943437e5adea35595cf867fc6c5c4f02795e1d7
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-11-20 09:10:00 -0600

    Fix #19 combine acceptedName field
    ``acceptedName`` field now always returns a name. If matched
    name was a current name -- it returns the matched name. If it was
    a synonym -- currently accepted name is returned.

--- spec/features/checklist_resolver_spec.rb
+++ spec/features/checklist_resolver_spec.rb
@@ -1,13 +1,34 @@
-describe "resolving variety of csv files" do
-  %i(all_fields sciname sciname_auth sciname_rank).each do |input|
-    context input do
-      it "resolves #{input}" do
-        output = "/tmp/#{input}-processed.csv"
-        input = FILES[input]
-        FileUtils.rm(output) if File.exist?(output)
-        GnCrossmap.run(input, output, 1, true)
-        expect(File.exist?(output)).to be true
+describe "features" do
+  context "resolving variety of csv files" do
+    %i(all_fields sciname sciname_auth sciname_rank).each do |input|
+      context input do
+        it "resolves #{input}" do
+          output = "/tmp/#{input}-processed.csv"
+          input = FILES[input]
+          FileUtils.rm(output) if File.exist?(output)
+          GnCrossmap.run(input, output, 1, true)
+          expect(File.exist?(output)).to be true
+        end
       end
+    end
+  end
+
+  context "combining acceptedName output" do
+    it "gives accepted name for all matches" do
+      output = "/tmp/output.csv"
+      input = FILES[:sciname]
+      GnCrossmap.run(input, output, 1, true)
+      CSV.open(output, col_sep: "\t", headers: true).each do |r|
+        next unless r["matchedEditDistance"] == "0"
+        expect(r["matchedName"].size).to be > 1
+        expect(r["acceptedName"].size).to be > 1
+        if r["synonymStatus"]
+          expect(r["matchedName"]).to_not eq r["acceptedName"]
+        else
+          expect(r["matchedName"]).to eq r["acceptedName"]
+        end
+      end
+      FileUtils.rm(output)
     end
   end
 end
